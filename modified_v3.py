8581f80327d797f01fc0ae427517c36b5757e57d
Woon Jia Hui <woonjiahui@gmail.com>
2021-11-01 20:02:25 +0800

    add modified_v3 files

--- modified_v3.py
+++ modified_v3.py
@@ -1,5 +1,5 @@
-# modified_v2.py
-# use original GoogLeNet inception module
+# modified_v3.py
+# add a new path to the inception module
 
 import tensorflow
 import pandas as pd
@@ -103,33 +103,46 @@ def inception_2(channel1, channel2, depth, layer_in):
 
   return layer_out
 
-def inception_3(channel1, channel2, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_5x5_reduce', layer_in)
-  layer_out = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_5x5', conv1)
+def inception_3(channel1, channel2, channel3, depth, layer_in):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_reduce', layer_in)
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1', conv1)
+  layer_out = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2', conv2)
 
   return layer_out
 
-def inception_4(channel1, depth, layer_in):
-  pool = MaxPooling2D(3, 1, 'same', name=f'nu_inception_{depth}_maxpool')(layer_in)
-  layer_out = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_1x1_0', pool)
+def inception_4(channel1, channel2, channel3, channel4, depth, layer_in):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_3_reduce', layer_in)
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_3', conv1)
+  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_3', conv2)
+  layer_out = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_3', conv3)
 
   return layer_out
 
-def nu_inception(params1, params2, params3, params4, direct_channel, depth, layer_in):
+def inception_5(channel1, channel2, channel3, channel4, channel5, depth, layer_in):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_4_reduce', layer_in)
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_4', conv1)
+  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_4', conv2)
+  conv4 = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_4', conv3)
+  layer_out = conv_bn_relu(channel5, 3, 1, 'same', f'nu_inception_{depth}_3x3_4_4', conv4)
+
+  return layer_out
+
+def nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, layer_in):
   out1 = inception_1(params1[0], depth, layer_in)
   out2 = inception_2(params2[0], params2[1], depth, layer_in)
-  out3 = inception_3(params3[0], params3[1], depth, layer_in)
-  out4 = inception_4(params4[0], depth, layer_in)
+  out3 = inception_3(params3[0], params3[1], params3[2], depth, layer_in)
+  out4 = inception_4(params4[0], params4[1], params4[2], params4[3], depth, layer_in)
+  out5 = inception_5(params5[0], params5[1], params5[2], params5[3], params5[4], depth, layer_in)
 
-  concat_layer = concatenate([out1, out2, out3, out4], axis=3)
+  concat_layer = concatenate([out1, out2, out3, out4, out5], axis=3)
 
   layer_out = conv_bn_relu(direct_channel, 1, 1, 'same', f'conv_direct_{depth}', concat_layer)
 
   return layer_out
 
-def resnet_block(params1, params2, params3, params4, direct_channel, bypass_channel, depth, layer_in):
+def resnet_block(params1, params2, params3, params4, params5, direct_channel, bypass_channel, depth, layer_in):
   pool = MaxPooling2D(3, 2, name=f'pool{depth}')(layer_in)
-  conv1 = nu_inception(params1, params2, params3, params4, direct_channel, depth, pool)
+  conv1 = nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, pool)
   conv2 = conv_bn_relu(bypass_channel, 1, 1, 'valid', f'conv_bypass_{depth}', pool)
 
   residual = Add(name=f'residual{depth}')([conv1, conv2])
@@ -137,10 +150,10 @@ def resnet_block(params1, params2, params3, params4, direct_channel, bypass_chan
 
   return layer_out
 
-module1 = [[16], [24, 32], [4, 8], [8]]
-module2 = [[32], [48, 64], [8, 16], [16]]
-module3 = [[64], [96, 128], [16, 32], [32]]
-module4 = [[128], [192, 256], [32, 64], [64]]
+module1 = [[16], [16, 24], [4, 8, 8], [4, 8, 8, 8], [4, 8, 8, 8, 8]]
+module2 = [[32], [32, 48], [8, 16, 16], [8, 16, 16, 16], [8, 16, 16, 16, 16]]
+module3 = [[64], [64, 96], [16, 32, 32], [16, 32, 32, 32], [16, 32, 32, 32, 32]]
+module4 = [[128], [128, 192], [32, 64, 64], [32, 64, 64, 64], [32, 64, 64, 64, 64]]
 modules = [module1, module2, module3, module4]
 
 direct_channels = [64, 128, 256, 512]
@@ -179,7 +192,7 @@ model = create_model()
 
 # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
 
-checkpoint_path = "modified_v2_training/cp.ckpt"
+checkpoint_path = "training_2/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
