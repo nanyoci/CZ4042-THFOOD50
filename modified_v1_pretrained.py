61563f8bc551e61aec4de67054e412fc04107b29
Woon Jia Hui <woonjiahui@gmail.com>
2021-11-09 17:27:54 +0800

    add v4, v1_pretrain and v4_pretrain

--- modified_v1_pretrained.py
+++ modified_v1_pretrained.py
@@ -1,18 +1,18 @@
-# modified_v3.py
-# add a new path to the inception module
+# modified_v1.py
+# removes reducers
+# pretrain on food101 dataset
 
-import tensorflow
-import pandas as pd
 import numpy as np
 import os
 import random
 
 # tensorflow libraries
 import tensorflow as tf
+import tensorflow_datasets as tfds
 import tensorflow.keras as keras
 from tensorflow.keras import layers, regularizers, Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, BatchNormalization, MaxPooling2D, Input, concatenate, GlobalAveragePooling2D, Rescaling, Activation, Add
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, BatchNormalization, MaxPooling2D, Input, concatenate, GlobalAveragePooling2D, Rescaling, Activation, Add, Resizing
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from keras.models import Model
 from keras.losses import SparseCategoricalCrossentropy
@@ -26,12 +26,14 @@ random.seed(SEED)
 np.random.seed(SEED)
 tf.random.set_seed(SEED)
 
-num_classes = 50
+num_classes = 101
 batch_size = 64
 lr = 0.1
 weight_decay = 0.0005
-epochs = 100
+epochs = 200
 momentum= 0.9
+
+pretrain_train, pretrain_val = tfds.load('food101', split=['train', 'validation'], shuffle_files=True, batch_size=batch_size, as_supervised=True)
 
 class_names = [x[0].split('/')[-1] for x in os.walk(dir+'/train')]
 class_names = class_names[1:]
@@ -65,21 +67,13 @@ test_ds = image_dataset_from_directory(
     image_size=(224,224)
 )
 
-# def augment(x,y):
-#   image = tf.image.random_brightness(x, max_delta=0.05)
-#   return x,y
-
-# train_ds = train_ds.map(augment)
-
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#   for i in range(9):
-#     ax = plt.subplot(3, 3, i + 1)
-#     plt.imshow(images[i].numpy().astype("uint8"))
-#     plt.title(class_names[labels[i]])
-#     plt.axis("off")
-
+resizing_layer = Resizing(224, 224)
 norm_layer = Rescaling(1./255)
+
+pretrain_train = pretrain_train.map(lambda x, y: (resizing_layer(x), y))
+pretrain_val = pretrain_val.map(lambda x, y: (resizing_layer(x), y))
+pretrain_train = pretrain_train.map(lambda x, y: (norm_layer(x), y))
+pretrain_val = pretrain_val.map(lambda x, y: (norm_layer(x), y))
 
 train_ds = train_ds.map(lambda x, y: (norm_layer(x), y))
 val_ds = val_ds.map(lambda x, y: (norm_layer(x), y))
@@ -97,52 +91,39 @@ def inception_1(channel1, depth, layer_in):
 
   return layer_out
 
-def inception_2(channel1, channel2, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_reduce', layer_in)
-  layer_out = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3', conv1)
+def inception_2(channel2, depth, layer_in):
+  layer_out = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3', layer_in)
 
   return layer_out
 
-def inception_3(channel1, channel2, channel3, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1', conv1)
+def inception_3(channel2, channel3, depth, layer_in):
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1', layer_in)
   layer_out = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2', conv2)
 
   return layer_out
 
-def inception_4(channel1, channel2, channel3, channel4, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_3_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_3', conv1)
+def inception_4(channel2, channel3, channel4, depth, layer_in):
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_3', layer_in)
   conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_3', conv2)
   layer_out = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_3', conv3)
 
   return layer_out
 
-def inception_5(channel1, channel2, channel3, channel4, channel5, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_4_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_4', conv1)
-  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_4', conv2)
-  conv4 = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_4', conv3)
-  layer_out = conv_bn_relu(channel5, 3, 1, 'same', f'nu_inception_{depth}_3x3_4_4', conv4)
-
-  return layer_out
-
-def nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, layer_in):
+def nu_inception(params1, params2, params3, params4, direct_channel, depth, layer_in):
   out1 = inception_1(params1[0], depth, layer_in)
-  out2 = inception_2(params2[0], params2[1], depth, layer_in)
-  out3 = inception_3(params3[0], params3[1], params3[2], depth, layer_in)
-  out4 = inception_4(params4[0], params4[1], params4[2], params4[3], depth, layer_in)
-  out5 = inception_5(params5[0], params5[1], params5[2], params5[3], params5[4], depth, layer_in)
+  out2 = inception_2(params2[0], depth, layer_in)
+  out3 = inception_3(params3[0], params3[1], depth, layer_in)
+  out4 = inception_4(params4[0], params4[1], params4[2], depth, layer_in)
 
-  concat_layer = concatenate([out1, out2, out3, out4, out5], axis=3)
+  concat_layer = concatenate([out1, out2, out3, out4], axis=3)
 
   layer_out = conv_bn_relu(direct_channel, 1, 1, 'same', f'conv_direct_{depth}', concat_layer)
 
   return layer_out
 
-def resnet_block(params1, params2, params3, params4, params5, direct_channel, bypass_channel, depth, layer_in):
+def resnet_block(params1, params2, params3, params4, direct_channel, bypass_channel, depth, layer_in):
   pool = MaxPooling2D(3, 2, name=f'pool{depth}')(layer_in)
-  conv1 = nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, pool)
+  conv1 = nu_inception(params1, params2, params3, params4, direct_channel, depth, pool)
   conv2 = conv_bn_relu(bypass_channel, 1, 1, 'valid', f'conv_bypass_{depth}', pool)
 
   residual = Add(name=f'residual{depth}')([conv1, conv2])
@@ -150,26 +131,29 @@ def resnet_block(params1, params2, params3, params4, params5, direct_channel, by
 
   return layer_out
 
-module1 = [[16], [16, 24], [4, 8, 8], [4, 8, 8, 8], [4, 8, 8, 8, 8]]
-module2 = [[32], [32, 48], [8, 16, 16], [8, 16, 16, 16], [8, 16, 16, 16, 16]]
-module3 = [[64], [64, 96], [16, 32, 32], [16, 32, 32, 32], [16, 32, 32, 32, 32]]
-module4 = [[128], [128, 192], [32, 64, 64], [32, 64, 64, 64], [32, 64, 64, 64, 64]]
+module1 = [[16], [32], [8, 8], [8, 8, 8]]
+module2 = [[32], [64], [16, 16], [16, 16, 16]]
+module3 = [[64], [128], [32, 32], [32, 32, 32]]
+module4 = [[128], [256], [64, 64], [64, 64, 64]]
 modules = [module1, module2, module3, module4]
 
 direct_channels = [64, 128, 256, 512]
 bypass_channels = [64, 128, 256, 512]
 
-def create_model():
+def create_model(fine_tuning = False):
   input = Input(shape=(224, 224, 3))
 
   layer_out = conv_bn_relu(64, 5, 2, 'same', 'conv1', input)
 
   for i, module in enumerate(modules):
-    layer_out = resnet_block(module[0], module[1], module[2], module[3], module[4], direct_channels[i], bypass_channels[i], i+1, layer_out)
+    layer_out = resnet_block(module[0], module[1], module[2], module[3], direct_channels[i], bypass_channels[i], i+1, layer_out)
 
   avg_pool = GlobalAveragePooling2D(name='avg_pool')(layer_out)
   
-  layer_out = Dense(num_classes)(avg_pool)
+  if fine_tuning:
+    layer_out = Dense(num_classes, name='dense_finetune')(avg_pool)
+  else:
+    layer_out = Dense(num_classes, name='dense')(avg_pool)
 
   model = Model(inputs=input, outputs=layer_out) 
 
@@ -185,14 +169,12 @@ def create_model():
 
 AUTOTUNE = tf.data.AUTOTUNE
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+pretrain_train = pretrain_train.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+pretrain_val = pretrain_val.cache().prefetch(buffer_size=AUTOTUNE)
 
 model = create_model()
 
-# keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
-
-checkpoint_path = "modified_v3_training/cp.ckpt"
+checkpoint_path = "modified_v1_pretrained/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
@@ -204,18 +186,40 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
 
 def scheduler(epoch, lr):
   rates = [0.1, 0.01,  0.001, 0.0001]
-  return tf.dtypes.cast(rates[epoch//25], tf.float32) 
+  return tf.dtypes.cast(rates[epoch//50], tf.float32) 
 
 sh_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
+# pre-train on food101 dataset
 start_time = time.time()
-history = model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=[cp_callback, sh_callback], verbose=2)
+history = model.fit(pretrain_train, epochs=epochs, validation_data=pretrain_val, callbacks=[cp_callback, sh_callback], verbose=2)
 time_taken = time.time() - start_time
-print("Total time taken to train in seconds:", time_taken)
+print("Total time taken to pre-train in seconds:", time_taken)
 
-os.listdir(checkpoint_dir)
+model = create_model(fine_tuning=False)
+model.load_weights(checkpoint_path) #load all weights
+x = model.layers[-2].output #remove 101 prediction layer
+num_classes=50
+predictions = Dense(num_classes, name='dense_finetune')(x)
+model = Model(inputs=model.input, outputs=predictions)
 
-model = create_model()
+lr = 0.0001
+epochs = 100
+
+model.compile(
+      optimizer= tf.keras.optimizers.SGD(learning_rate=lr, momentum=momentum),
+      loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+      metrics=['accuracy'],
+  )
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+# fine-tune on THFOOD50 dataset
+start_time = time.time()
+history = model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=[cp_callback], verbose=2)
+time_taken = time.time() - start_time
+print("Total time taken to fine-tune in seconds:", time_taken)
 
 model.load_weights(checkpoint_path)
 
--- modified_v4.py
+++ modified_v4.py
@@ -1,5 +1,6 @@
-# modified_v3.py
-# add a new path to the inception module
+# modified_v4.py
+# 8 inception blocks plus new skip connection from 4th block to the end
+# inception block #5 - #8 have dropout layers
 
 import tensorflow
 import pandas as pd
@@ -12,12 +13,14 @@ import tensorflow as tf
 import tensorflow.keras as keras
 from tensorflow.keras import layers, regularizers, Sequential
 from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
-from tensorflow.keras.layers import Dense, Dropout, Conv2D, BatchNormalization, MaxPooling2D, Input, concatenate, GlobalAveragePooling2D, Rescaling, Activation, Add
+from tensorflow.keras.layers import Dense, Dropout, Conv2D, BatchNormalization, MaxPooling2D, Input, concatenate, GlobalAveragePooling2D, Rescaling, Activation, Add, ZeroPadding2D
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 from keras.models import Model
 from keras.losses import SparseCategoricalCrossentropy
 
 import time
+
+from tensorflow.python.keras.backend import dropout
 
 dir = '/home/UG/jwoon006/CZ4042-THFOOD50/THFOOD50-v1'
 
@@ -32,6 +35,7 @@ lr = 0.1
 weight_decay = 0.0005
 epochs = 100
 momentum= 0.9
+dropout_rate = 0.1
 
 class_names = [x[0].split('/')[-1] for x in os.walk(dir+'/train')]
 class_names = class_names[1:]
@@ -85,75 +89,74 @@ train_ds = train_ds.map(lambda x, y: (norm_layer(x), y))
 val_ds = val_ds.map(lambda x, y: (norm_layer(x), y))
 test_ds = test_ds.map(lambda x, y: (norm_layer(x), y))
 
-def conv_bn_relu(num_filters, filter_size, stride, padding, name, layer_in):
+def conv_bn_relu(num_filters, filter_size, stride, padding, name, layer_in, dropout = False):
   conv = Conv2D(num_filters, filter_size, stride, padding=padding, name=name, kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2(weight_decay),)(layer_in)
   conv_bn = BatchNormalization()(conv)
-  layer_out = Activation('relu')(conv_bn)
+  if dropout:
+    conv_bn_do = Dropout(dropout_rate)(conv_bn)
+    layer_out = Activation('relu')(conv_bn_do)
+  else:
+    layer_out = Activation('relu')(conv_bn)
 
   return layer_out
 
-def inception_1(channel1, depth, layer_in):
-  layer_out = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_1x1', layer_in)
+def inception_1(channel1, depth, layer_in, dropout = False):
+  layer_out = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_1x1', layer_in, dropout)
 
   return layer_out
 
-def inception_2(channel1, channel2, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_reduce', layer_in)
-  layer_out = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3', conv1)
+def inception_2(channel1, channel2, depth, layer_in, dropout = False):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_reduce', layer_in, dropout)
+  layer_out = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3', conv1, dropout)
 
   return layer_out
 
-def inception_3(channel1, channel2, channel3, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1', conv1)
-  layer_out = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2', conv2)
+def inception_3(channel1, channel2, channel3, depth, layer_in, dropout = False):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_reduce', layer_in, dropout)
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1', conv1, dropout)
+  layer_out = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2', conv2, dropout)
 
   return layer_out
 
-def inception_4(channel1, channel2, channel3, channel4, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_3_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_3', conv1)
-  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_3', conv2)
-  layer_out = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_3', conv3)
+def inception_4(channel1, channel2, channel3, channel4, depth, layer_in, dropout = False):
+  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_3_reduce', layer_in, dropout)
+  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_3', conv1, dropout)
+  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_3', conv2, dropout)
+  layer_out = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_3', conv3, dropout)
 
   return layer_out
 
-def inception_5(channel1, channel2, channel3, channel4, channel5, depth, layer_in):
-  conv1 = conv_bn_relu(channel1, 1, 1, 'valid', f'nu_inception_{depth}_3x3_0_4_reduce', layer_in)
-  conv2 = conv_bn_relu(channel2, 3, 1, 'same', f'nu_inception_{depth}_3x3_1_4', conv1)
-  conv3 = conv_bn_relu(channel3, 3, 1, 'same', f'nu_inception_{depth}_3x3_2_4', conv2)
-  conv4 = conv_bn_relu(channel4, 3, 1, 'same', f'nu_inception_{depth}_3x3_3_4', conv3)
-  layer_out = conv_bn_relu(channel5, 3, 1, 'same', f'nu_inception_{depth}_3x3_4_4', conv4)
+def nu_inception(params1, params2, params3, params4, direct_channel, depth, layer_in, dropout = False):
+  out1 = inception_1(params1[0], depth, layer_in, dropout)
+  out2 = inception_2(params2[0], params2[1], depth, layer_in, dropout)
+  out3 = inception_3(params3[0], params3[1], params3[2], depth, layer_in, dropout)
+  out4 = inception_4(params4[0], params4[1], params4[2], params4[3], depth, layer_in, dropout)
+
+  concat_layer = concatenate([out1, out2, out3, out4], axis=3)
+
+  layer_out = conv_bn_relu(direct_channel, 1, 1, 'same', f'conv_direct_{depth}', concat_layer, dropout)
 
   return layer_out
 
-def nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, layer_in):
-  out1 = inception_1(params1[0], depth, layer_in)
-  out2 = inception_2(params2[0], params2[1], depth, layer_in)
-  out3 = inception_3(params3[0], params3[1], params3[2], depth, layer_in)
-  out4 = inception_4(params4[0], params4[1], params4[2], params4[3], depth, layer_in)
-  out5 = inception_5(params5[0], params5[1], params5[2], params5[3], params5[4], depth, layer_in)
-
-  concat_layer = concatenate([out1, out2, out3, out4, out5], axis=3)
-
-  layer_out = conv_bn_relu(direct_channel, 1, 1, 'same', f'conv_direct_{depth}', concat_layer)
-
-  return layer_out
-
-def resnet_block(params1, params2, params3, params4, params5, direct_channel, bypass_channel, depth, layer_in):
-  pool = MaxPooling2D(3, 2, name=f'pool{depth}')(layer_in)
-  conv1 = nu_inception(params1, params2, params3, params4, params5, direct_channel, depth, pool)
-  conv2 = conv_bn_relu(bypass_channel, 1, 1, 'valid', f'conv_bypass_{depth}', pool)
+def resnet_block(params1, params2, params3, params4, direct_channel, bypass_channel, depth, layer_in, last_4):
+  if last_4:
+    #keep the shape at (6, 6, numFilters)
+    pad = ZeroPadding2D(3)(layer_in)
+    pool = MaxPooling2D(3, 2, padding='same', name=f'pool{depth}')(pad)
+  else:
+    pool = MaxPooling2D(3, 2, name=f'pool{depth}')(layer_in)
+  conv1 = nu_inception(params1, params2, params3, params4, direct_channel, depth, pool, dropout = last_4)
+  conv2 = conv_bn_relu(bypass_channel, 1, 1, 'valid', f'conv_bypass_{depth}', pool) # no dropout here, we want to save all the information from the skip connection
 
   residual = Add(name=f'residual{depth}')([conv1, conv2])
   layer_out = Activation('relu')(residual)
 
   return layer_out
 
-module1 = [[16], [16, 24], [4, 8, 8], [4, 8, 8, 8], [4, 8, 8, 8, 8]]
-module2 = [[32], [32, 48], [8, 16, 16], [8, 16, 16, 16], [8, 16, 16, 16, 16]]
-module3 = [[64], [64, 96], [16, 32, 32], [16, 32, 32, 32], [16, 32, 32, 32, 32]]
-module4 = [[128], [128, 192], [32, 64, 64], [32, 64, 64, 64], [32, 64, 64, 64, 64]]
+module1 = [[16], [24, 32], [4, 8, 8], [4, 8, 8, 8]]
+module2 = [[32], [48, 64], [8, 16, 16], [8, 16, 16, 16]]
+module3 = [[64], [96, 128], [16, 32, 32], [16, 32, 32, 32]]
+module4 = [[128], [192, 256], [32, 64, 64], [32, 64, 64, 64]]
 modules = [module1, module2, module3, module4]
 
 direct_channels = [64, 128, 256, 512]
@@ -162,10 +165,18 @@ bypass_channels = [64, 128, 256, 512]
 def create_model():
   input = Input(shape=(224, 224, 3))
 
-  layer_out = conv_bn_relu(64, 5, 2, 'same', 'conv1', input)
+  first_4_out = conv_bn_relu(64, 5, 2, 'same', 'conv1', input)
 
+  # first 4 inception modules
   for i, module in enumerate(modules):
-    layer_out = resnet_block(module[0], module[1], module[2], module[3], module[4], direct_channels[i], bypass_channels[i], i+1, layer_out)
+    first_4_out = resnet_block(module[0], module[1], module[2], module[3], direct_channels[i], bypass_channels[i], i+1, first_4_out, last_4=False)
+  # last 4 inception modules
+  last_4_out = conv_bn_relu(64, 1, 1, 'same', 'convolution_reduceNumOfFilters', first_4_out, dropout=False) # change number of filters from 512 to 64
+  for i, module in enumerate(modules):
+    last_4_out = resnet_block(module[0], module[1], module[2], module[3], direct_channels[i], bypass_channels[i], i+5, last_4_out, last_4=True)
+
+  residual = Add(name='residual9')([first_4_out, last_4_out])
+  layer_out = Activation('relu')(residual)
 
   avg_pool = GlobalAveragePooling2D(name='avg_pool')(layer_out)
   
@@ -192,7 +203,7 @@ model = create_model()
 
 # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
 
-checkpoint_path = "modified_v3_training/cp.ckpt"
+checkpoint_path = "modified_v4_training/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
 cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
