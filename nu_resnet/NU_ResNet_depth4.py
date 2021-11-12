f9f078c833334bf6347ecaf1acacb02763fff553
nanyoci <44506561+nanyoci@users.noreply.github.com>
2021-11-12 18:45:04 +0800

    Added doc

--- nu_resnet/NU_ResNet_depth4.py
+++ nu_resnet/NU_ResNet_depth4.py
@@ -14,6 +14,7 @@ from tensorflow.keras.preprocessing import image_dataset_from_directory
 from keras.models import Model
 from keras.losses import SparseCategoricalCrossentropy
 
+# change to path to THFOOD50 directory
 dir = '../THFOOD50-v1'
 
 SEED = 50
@@ -31,6 +32,7 @@ momentum= 0.9
 class_names = [x[0].split('/')[-1] for x in os.walk(dir+'/train')]
 class_names = class_names[1:]
 
+# load data
 train_ds = image_dataset_from_directory(
     dir+'/train',
     labels='inferred',
@@ -60,26 +62,14 @@ test_ds = image_dataset_from_directory(
     image_size=(224,224)
 )
 
-# def augment(x,y):
-#   image = tf.image.random_brightness(x, max_delta=0.05)
-#   return x,y
-
-# train_ds = train_ds.map(augment)
-
-# plt.figure(figsize=(10, 10))
-# for images, labels in train_ds.take(1):
-#   for i in range(9):
-#     ax = plt.subplot(3, 3, i + 1)
-#     plt.imshow(images[i].numpy().astype("uint8"))
-#     plt.title(class_names[labels[i]])
-#     plt.axis("off")
-
+# normalize
 norm_layer = Rescaling(1./255)
 
 train_ds = train_ds.map(lambda x, y: (norm_layer(x), y))
 val_ds = val_ds.map(lambda x, y: (norm_layer(x), y))
 test_ds = test_ds.map(lambda x, y: (norm_layer(x), y))
 
+# NU-InNet module
 def conv_bn_relu(num_filters, filter_size, stride, padding, name, layer_in):
   conv = Conv2D(num_filters, filter_size, stride, padding=padding, name=name, kernel_initializer='glorot_normal', kernel_regularizer=regularizers.l2(weight_decay),)(layer_in)
   conv_bn = BatchNormalization()(conv)
@@ -125,6 +115,7 @@ def nu_inception(params1, params2, params3, params4, direct_channel, depth, laye
 
   return layer_out
 
+# NU-ResNet
 def resnet_block(params1, params2, params3, params4, direct_channel, bypass_channel, depth, layer_in):
   pool = MaxPooling2D(3, 2, name=f'pool{depth}')(layer_in)
   conv1 = nu_inception(params1, params2, params3, params4, direct_channel, depth, pool)
@@ -135,6 +126,7 @@ def resnet_block(params1, params2, params3, params4, direct_channel, bypass_chan
 
   return layer_out
 
+# model parameters
 module1 = [[16], [24, 32], [4, 8, 8], [4, 8, 8, 8]]
 module2 = [[32], [48, 64], [8, 16, 16], [8, 16, 16, 16]]
 module3 = [[64], [96, 128], [16, 32, 32], [16, 32, 32, 32]]
@@ -144,6 +136,7 @@ modules = [module1, module2, module3, module4]
 direct_channels = [64, 128, 256, 512]
 bypass_channels = [64, 128, 256, 512]
 
+# function to create NU-ResNet of depth 4
 def create_model():
   input = Input(shape=(224, 224, 3))
 
@@ -177,6 +170,7 @@ model = create_model()
 
 # keras.utils.plot_model(model, show_shapes=True, rankdir="LR")
 
+# checkpoint to save best model
 checkpoint_path = "training_2/cp.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
 
@@ -187,16 +181,19 @@ cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
                                                  mode='min',
                                                  verbose=1)
 
+# LR scheduler as specified in the paper
 def scheduler(epoch, lr):
   rates = [0.1, 0.01,  0.001, 0.0001]
   return tf.dtypes.cast(rates[epoch//25], tf.float32) 
 
 sh_callback = tf.keras.callbacks.LearningRateScheduler(scheduler)
 
+# train model
 history = model.fit(train_ds, epochs=epochs, validation_data=val_ds, callbacks=[cp_callback, sh_callback], verbose=2)
 
 os.listdir(checkpoint_dir)
 
+# create model for test set
 model = create_model()
 
 model.load_weights(checkpoint_path)
